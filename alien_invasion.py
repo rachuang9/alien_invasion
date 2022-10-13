fbee9d4617f378935e9efd0cf6e4013018e84dd3
rachuang9 <m252646@usna.edu>
2022-10-13 09:38:00 -0400

    fixing formatt

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,24 +1,31 @@
 import pygame
 import sys
-from settings import settings
+from settings import Settings
+from ship import Ship
+
+
 class AlienInvasion:
 
-    def _init_(self):
+    def __init__(self):
         pygame.init()
-        self.settings=settings()
-        self.screen=pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color=(230,230,230)
+        self.ship = Ship(self)
+
+        self.bg_color = (230, 230, 230)
+
     def run_game(self):
 
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.Quit:
                     sys.exit()
-            self.screen.fill(self.settingss.bg.color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
 
 
-if _name_== '_main_':
+if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
--- game.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import sys
-class AlienInvasion:
-    """Overall class to manage game assets and behavior"""
-    def_init_(self):
-        """Initialize the game, and create game resources"""
-        pygame.init()
